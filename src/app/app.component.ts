155757a6a59b4b81e5ba5425747b3da09a333fea
pedroccg <pedro.guimaraes@ccg.pt>
2023-04-26 12:50:42 +0100

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,216 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { interval, Subject, switchMap, takeUntil } from 'rxjs';
+import { VisNetworkService, Data, DataSet, Node, Options, Edge } from 'ngx-vis';
+//import * as d3 from 'd3'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'airflow-gateway';
+export class AppComponent implements OnInit {
+
+
+  public visNetworkOptions!: Options
+
+
+
+  fileContent: string | ArrayBuffer | null = null;
+
+  dagId: string = 'spark_cd_to_lpg';
+  dagRunId: string = '';
+  results = '';
+  subscription: any;
+
+  jsonString = ''
+
+  jsonData: any;
+  visData_1: any;
+  visData_2: any;
+
+  readyToShow: boolean = false;
+  visNetworks: any[] = [];
+  loadingData: boolean = false;
+
+
+  
+
+  public networkInitialized(): void {
+    
+  }
+
+  constructor(private http: HttpClient, private visNetworkService: VisNetworkService) {
+
+    this.visNetworkOptions = {
+      interaction:{
+        zoomView: true,
+      },
+      physics: true,
+      
+    }
+ 
+  }
+
+  ngOnInit() {
+
+  
+
+  }
+
+  loadJsonData() {
+    this.http.get<any[]>('assets/jsons/tpch.json').subscribe((jsonDatas) => {
+      jsonDatas.forEach((jsonData, index) => {
+        const visData = this.convertJsonToVisData(jsonData);
+        const nodes = visData.nodes;
+        const edges = visData.edges;
+        const name = jsonData.pattern;
+  
+        this.visNetworks.push({
+          name: name,
+          id: `visNetwork_${index + 1}`,
+          data: { nodes: nodes, edges: edges },
+          options: {
+            // ... your options for the instance
+          },
+        });
+      });
+      this.readyToShow = true;
+    });
+  }
+
+  public ngOnDestroy(): void {
+    //this.visNetworkService.off(this.visNetwork, 'click');
+  }
+
+  private createAuthorizationHeader(): HttpHeaders {
+    const username = 'airflow';
+    const password = 'airflow';
+    const encodedCredentials = btoa(`${username}:${password}`);
+    const headers = new HttpHeaders().set('Authorization', `Basic ${encodedCredentials}`);
+    return headers;
+  }
+
+  startDag() {
+    const url = 'http://172.16.131.117:8080/api/v1/dags/spark_cd_to_lpg/dagRuns';
+    const headers = this.createAuthorizationHeader();
+    return this.http.post(url, {}, { headers }).subscribe(
+      res => console.log('Job started successfully', res),
+      err => console.error('Error starting job', err)
+    );
+  }
+
+  convertJsonToVisData(jsonData: any) {
+    const nodesData = jsonData.nodes.map((node: any) => ({
+      id: node.id,
+      label: node.label,
+      title: 'Title of Node 5' 
+    }));
+
+    const edgesData = jsonData.edges.map((edge: any) => ({
+      id: edge.id,
+      from: edge.source,
+      to: edge.target,
+      label: edge.label,
+      arrows: "to"
+    }));
+
+    return {
+      nodes: new DataSet(nodesData),
+      edges: new DataSet(edgesData)
+    };
+  }
+
+  async startJob(): Promise<void> {
+
+    if (!this.fileContent) {
+      return;
+    }
+
+    const dagId = 'spark_cd_to_lpg';
+    const airflowUrl = 'http://172.16.131.117:8080';
+    const apiUrl = `/api/v1/dags/${dagId}/dagRuns`;
+    const headers = this.createAuthorizationHeader();
+
+    const conf = {
+      "conf": {
+        "file_content": this.fileContent
+      }
+    };
+
+    this.http.post(`${airflowUrl}${apiUrl}`, conf, { headers }).subscribe({
+      next: (res: any) => {
+        this.dagRunId = res.dag_run_id
+        console.log('Job started successfully:', res.dag_run_id);
+      },
+      error: (err: any) => {
+        console.error('Error starting job', err);
+      }
+    });
+  }
+
+  getDagRun(dagId: string, dagRunId: string): void {
+    const airflowUrl = 'http://172.16.131.117:8080';
+    const apiUrl = `/api/v1/dags/${dagId}/dagRuns/${dagRunId}`;
+
+    // Replace 'username' and 'password' with your actual credentials
+    const username = 'airflow';
+    const password = 'airflow';
+    const base64Credentials = btoa(`${username}:${password}`);
+
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': `Basic ${base64Credentials}`,
+    });
+
+    // Check every 5 seconds
+    const checkInterval = 1000;
+    const stopChecking$ = new Subject<void>();
+
+    this.loadingData = true;
+
+
+    interval(checkInterval)
+      .pipe(
+        takeUntil(stopChecking$), // Stop checking when the job is finished
+        // Switch to the GET request for each interval tick
+        switchMap(() => this.http.get(`${airflowUrl}${apiUrl}`, { headers }))
+      )
+      .subscribe({
+        next: (res: any) => {
+          if (res.state === 'success') {
+            // when dag is over
+            this.loadJsonData();
+            stopChecking$.next(); // Stop checking when the job is finished
+          }
+          else if (res.state === 'failed') { 
+            this.loadingData = false;
+            
+          } else {
+            console.log('Job is not finished yet, current state:', res.state);
+          }
+        },
+        error: (err: any) => {
+          console.error('Error fetching DAG run details', err);
+        }
+      });
+  }
+
+
+  title = 'An Automated Patterns-based Model-to-Model Mapping and Transformation System for Labeled Property Graphs';
+  selectedFile: File | null = null;
+
+  onFileSelected(event: Event): void {
+    const file = (event.target as HTMLInputElement).files?.[0];
+    if (!file) {
+      return;
+    }
+
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.fileContent = reader.result;
+    };
+    reader.readAsText(file);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { VisModule } from 'ngx-vis';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { HttpClientModule } from '@angular/common/http';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +22,16 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    HttpClientModule,
+    VisModule,
+    MatProgressSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
